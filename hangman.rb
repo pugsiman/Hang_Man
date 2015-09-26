c88f99604758a4c8baf60dbcc158a366ad0dc89c
Aviv Levinski <pugsiman@gmail.com>
2015-09-26 23:47:19 +0300

    Added load and save features, and various optimizations.

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 
-# main hangman class
+# Main hangman class
 class Game
   def initialize
     @selected_word = load_word
@@ -9,25 +9,26 @@ class Game
     play
   end
 
+  def play
+    puts "\n      Welcome to hangman."
+    puts '      Would you like to load an existing game, or start a new one?'
+    print '      Type load, new or exit: '
+    game_choice(gets.chomp.downcase)
+  end
+
   def new_game
+    puts "\e[H\e[2J"
     game_progress until !@displayed_word.include?('_ ') || @guesses.length == 10
-    if !@displayed_word.include?('_ ')
-      puts 'Nice job, you\'ve won.'
-      end_game
-    elsif @guesses.length == 10
-      puts 'No wonder people think you\'re stupid, you\'ve lost.'
+    if @guesses.length == 10
+      puts "\n      No wonder people think you\'re silly, you\'ve lost."
+      puts "\n      The word was #{@selected_word}."
+    else
+      puts "\n      Nice job, you\'ve won."
+      puts "\n      The word was indeed #{@selected_word}."
     end
   end
 
   private
-
-  def play
-    puts "\n      Welcome to hangman."
-    puts '      Would you like to load an existing game, or start a new one?'
-    puts '      Type load, new or exit'
-    print "\n      "
-    game_choice(gets.chomp.downcase)
-  end
 
   def game_progress
     print "\n\n\n\n\n      #{@displayed_word.join} (#{@displayed_word.length})"
@@ -39,38 +40,39 @@ class Game
 
   def load_word
     dictionary = File.readlines '5desk.txt'
-    dictionary.select { |x| 4 < x.length && x.length < 13 }.sample.delete!("\n")
+    # a random word from an array of 5 to 12 characters-long words.
+    dictionary.select { |x| x.length.between?(5, 13) }.sample.delete!("\n")
   end
 
   def game_choice(choice)
     case choice
     when 'new'  then new_game
     when 'load' then load_game
-    when 'exit' then exit_game
+    when 'exit' then puts '      Wow, rude. Bye.'
     else
-      puts '      You silly, you.'
+      puts '      You silly goose, you. Try again.'
       play
     end
   end
 
   def make_turn
-    puts "\n      Choose a letter to guess, or type EXIT:"
-    print "\n      "
+    print "\n      Choose a letter to guess, or type EXIT: "
     validate_input(gets.chomp.downcase)
   end
 
   def validate_input(input)
-    if input == 'save'
-      save_game
-    elsif input == 'exit'
-      exit_game
-    elsif @guesses.include?(input)
-      puts '      Already used that letter.'
+    if @guesses.include?(input)
+      puts '      Already used that letter, monkey.'
       make_turn
     elsif (/[A-Za-z]/).match(input) && input.length == 1
       input
+    elsif input == 'save'
+      save_game
+    elsif input == 'exit'
+      puts "      Wow, rude. Bye."
+      exit
     else
-      puts '      You can only enter a single letter.'
+      puts '      You can only enter a letter, and a single one at that.'
       make_turn
     end
   end
@@ -90,27 +92,26 @@ class Game
     puts "\n      You have #{10 - @guesses.size} tries left."
   end
 
-  def end_game
-    puts "\n      The word is #{@selected_word}"
-  end
+  # Hangman visualization
+  def visual_status; end
 
-  def exit_game
-    puts "      Wow, rude. Bye.\n"
+  # Save and load methods
+  def save_game
+    Dir.mkdir('saves') unless Dir.exist? 'saves'
+    File.open('saves/save.yaml', 'w') do |file|
+      file << YAML.dump(self)
+    end
+    puts '      Game has been saved'
     exit
   end
 
-  # hangman visualization
-  def visual_status
-    
-  end
-
-  # save and load methods
-  def save_game
-    Dir.mkdir('saves') unless Dir.exist? 'saves'
-  end
-
   def load_game
-    
+    if File.exist?('saves/save.yaml')
+      data = File.open('saves/save.yaml', &:read) # May not work for older versions of ruby
+      YAML.load(data).new_game
+    else
+      puts 'You have no save file. Are you feeling alright?'
+    end
   end
 end
 
