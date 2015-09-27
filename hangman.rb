ca7a1a087a80ee4595170e65216e6456e8a1e1a3
Aviv Levinski <pugsiman@gmail.com>
2015-09-27 04:53:18 +0300

    Various performance and maintainability optimizations

--- hangman.rb
+++ hangman.rb
@@ -1,15 +1,24 @@
 require 'yaml'
 
-# Main hangman class
+# Adding method to Array that removes spaces inside scope of the string.
+class Array
+  def unspace
+    join.gsub(/[[:space:]]/, '')
+  end
+end
+
+# Hangman game class
 class Game
   def initialize
     @selected_word = load_word
     @guesses = []
     @displayed_word = Array.new(@selected_word.length, '_ ')
-    play
+    start_screen
   end
 
-  def play
+  protected
+
+  def start_screen
     puts "\n      Welcome to hangman."
     puts '      Would you like to load an existing game, or start a new one?'
     print '      Type load, new or exit: '
@@ -17,32 +26,19 @@ class Game
   end
 
   def new_game
-    puts "\e[H\e[2J"
-    game_progress until !@displayed_word.include?('_ ') || @guesses.length == 10
-    if @guesses.length == 10
+    puts "\e[H\e[2J" # using the clear command through ASCII.
+    puts @selected_word
+    progress until win_game?
+    if @guesses.size == 10
       puts "\n      No wonder people think you\'re silly, you\'ve lost."
-      puts "\n      The word was #{@selected_word}."
+      puts "\n      The word was '#{@selected_word}'."
     else
       puts "\n      Nice job, you\'ve won."
-      puts "\n      The word was indeed #{@selected_word}."
+      puts "\n      The word was indeed '#{@selected_word}'."
     end
   end
 
   private
-
-  def game_progress
-    print "\n\n\n\n\n      #{@displayed_word.join} (#{@displayed_word.length})"
-    print "#{visual_status}\n"
-    display_guesses
-    @guess = make_turn
-    validate_letter
-  end
-
-  def load_word
-    dictionary = File.readlines '5desk.txt'
-    # a random word from an array of 5 to 12 characters-long words.
-    dictionary.select { |x| x.length.between?(5, 13) }.sample.delete!("\n")
-  end
 
   def game_choice(choice)
     case choice
@@ -51,33 +47,51 @@ class Game
     when 'exit' then puts '      Wow, rude. Bye.'
     else
       puts '      You silly goose, you. Try again.'
-      play
+      start_screen
     end
   end
 
-  def make_turn
+  def progress
+    print "\n\n\n\n\n      #{@displayed_word.join} (#{@displayed_word.length})"
+    # print "#{visual_status}\n"
+    puts "\n      Incorrect guesses: [#{@guesses.join(', ')}]" unless @guesses.empty?
+    puts "\n      You have #{10 - @guesses.size} tries left."
     print "\n      Choose a letter to guess, or type EXIT: "
-    validate_input(gets.chomp.downcase)
+    @guess = validate_input(gets.chomp.downcase)
+    check_guess
+  end
+
+  def load_word
+    dictionary = File.readlines '5desk.txt'
+    # A random word from an array of 5 to 12 characters-long words.
+    normal_range = proc { |x| x.length.between?(5, 12) }
+    dictionary.select(&normal_range).sample.downcase.delete!("\n")
   end
 
   def validate_input(input)
     if @guesses.include?(input)
-      puts '      Already used that letter, monkey.'
-      make_turn
-    elsif (/[A-Za-z]/).match(input) && input.length == 1
+      puts '      \nAlready used that letter, monkey.'
+      revalidate
+    elsif (/[a-z]/).match(input) && input.length == 1
       input
     elsif input == 'save'
       save_game
     elsif input == 'exit'
-      puts "      Wow, rude. Bye."
+      puts '      \nWow, rude. Bye.'
       exit
     else
-      puts '      You can only enter a letter, and a single one at that.'
-      make_turn
+      puts '      \nYou can only enter a letter, and a single one at that.'
+      revalidate
     end
   end
 
-  def validate_letter
+  # Fail-safe (re)method to avoid recursively applying Array methods. 
+  def revalidate
+    validate_input(gets.chomp.downcase)
+  end
+
+  # Display the letter if the selected word containts it.
+  def check_guess
     if @selected_word.split('').include?(@guess)
       @selected_word.split('').each_with_index do |letter, idx|
         @displayed_word[idx] = "#{letter} " if @guess == letter
@@ -87,9 +101,9 @@ class Game
     end
   end
 
-  def display_guesses
-    puts "\n      Incorrect guesses: [#{@guesses.join(', ')}]" unless @guesses.empty?
-    puts "\n      You have #{10 - @guesses.size} tries left."
+  # Check for win sequence.
+  def win_game?
+    @displayed_word.unspace == @selected_word || @guesses.size == 10
   end
 
   # Hangman visualization
@@ -107,7 +121,8 @@ class Game
 
   def load_game
     if File.exist?('saves/save.yaml')
-      data = File.open('saves/save.yaml', &:read) # May not work for older versions of ruby
+      # May not work for older versions of ruby
+      data = File.open('saves/save.yaml', &:read)
       YAML.load(data).new_game
     else
       puts 'You have no save file. Are you feeling alright?'
